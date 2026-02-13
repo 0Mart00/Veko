f195f4ef4b66c568fa7ef0ceb4439e7b72ed3a48
0Mart00 <nyiri.martin.101@gmail.com>
2026-02-13 16:01:27 +0100

    T-object bug fix

--- core/main.c
+++ core/main.c
@@ -2,44 +2,58 @@
 #include <stdlib.h>
 #include <dlfcn.h>
 #include <unistd.h>
-#include <string.h>
+#include <sys/stat.h>
+#include <time.h>
 #include "../include/interface.h"
 
 const char* MODULE_PATH = "./build/logic.so";
 
-int main() {
-    // Allocate EngineState on heap instead of stack (33 MB is too large for stack)
-    EngineState* global_state = (EngineState*)calloc(1, sizeof(EngineState));
-    if (!global_state) {
-        fprintf(stderr, "Failed to allocate memory for EngineState\n");
-        return 1;
+// Függvény a fájl utolsó módosítási idejének lekérdezésére
+time_t get_file_time(const char* path) {
+    struct stat attr;
+    if (stat(path, &attr) == 0) {
+        return attr.st_mtime;
     }
-    
+    return 0;
+}
+
+int main() {
+    EngineState global_state = {0};
     void* handle = NULL;
     logic_update_fn update = NULL;
+    time_t last_mod_time = 0;
 
     printf("--- Dynamic Logic Engine Started ---\n");
 
     while (1) {
-        // Megpróbáljuk betölteni a modult
-        handle = dlopen(MODULE_PATH, RTLD_NOW);
-        
-        if (!handle) {
-            fprintf(stderr, "Waiting for module: %s\n", dlerror());
-        } else {
-            update = (logic_update_fn)dlsym(handle, "update");
-            
-            if (update) {
-                update(global_state); // Átadjuk a memóriát a modulnak
+        time_t current_mod_time = get_file_time(MODULE_PATH);
+
+        // Csak akkor töltjük újra a modult, ha a fájl megváltozott (hot-reload)
+        if (current_mod_time > last_mod_time) {
+            if (handle) {
+                dlclose(handle); // Zárjuk be a régit
+                printf(">>> [HOT-RELOAD] Reloading module...\n");
             }
             
-            dlclose(handle); // Bezárjuk, hogy a fájl felülírható legyen
+            handle = dlopen(MODULE_PATH, RTLD_NOW);
+            
+            if (!handle) {
+                fprintf(stderr, ">>> [ERROR] Waiting for module: %s\n", dlerror());
+            } else {
+                update = (logic_update_fn)dlsym(handle, "update");
+                last_mod_time = current_mod_time;
+            }
         }
 
-        global_state->info.frame_count++; 
-        usleep(500000); 
+        // Ha a modul be van töltve, folyamatosan futtatjuk az update-et
+        if (update) {
+            update(&global_state);
+        }
+
+        global_state.info.frame_count++; 
+        
+        // Ciklus sebessége (16ms = kb. 60 FPS a GUI-nak)
+        usleep(16000); 
     }
-    
-    free(global_state);
     return 0;
 }
--- include/interface.h
+++ include/interface.h
@@ -1,12 +1,14 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-#define MAX_VARS 100
+#define MAX_VARS 200
 #define MAX_FUNCS 50
-#define MAX_CLASSES 10
-#define MAX_OBJECTS 50  // Új: ennyi aktív objektumunk lehet
-#define MAX_LINES 20
-#define NAME_LEN 32
+#define MAX_CLASSES 20
+#define MAX_OBJECTS 50
+#define MAX_LINES 50
+#define MAX_LIST_SIZE 100
+#define MAX_IMPORTS 10
+#define NAME_LEN 64
 
 typedef struct {
     double wealth;
@@ -16,51 +18,89 @@ typedef struct {
 typedef enum {
     T_UNDEFINED = 0,
     T_NUMBER,
-    T_STRING
+    T_STRING,
+    T_LIST,
+    T_DICT,
+    T_BOOL,
+    T_OBJECT
 } VarType;
 
+// Lista típus
+typedef struct {
+    double items[MAX_LIST_SIZE];
+    int size;
+} VarList;
+
+// Dictionary típus
+typedef struct {
+    char keys[MAX_LIST_SIZE][NAME_LEN];
+    double values[MAX_LIST_SIZE];
+    int size;
+} VarDict;
+
+// Változó struktúra
 typedef struct {
     char name[NAME_LEN];
     VarType type;
     
-    // Union: vagy számot, vagy szöveget tárol, egyszerre csak egyet
     union {
         double num_val;
-        char str_val[64];
+        char str_val[128];
+        VarList list_val;
+        VarDict dict_val;
+        int bool_val;
     } data;
 } CustomVar;
+
+// Függvény struktúra
 typedef struct {
     char name[NAME_LEN];
+    char params[10][NAME_LEN];  // Paraméterek
+    int param_count;
     char lines[MAX_LINES][128];
     int line_count;
-    int arg_count; 
 } CustomFunc;
 
+// Osztály struktúra
 typedef struct {
     char name[NAME_LEN];
-    char parent[NAME_LEN]; // Öröklődéshez
+    char parent[NAME_LEN];
+    CustomVar attributes[50];   // Osztály attribútumok
+    int attr_count;
     CustomFunc methods[MAX_FUNCS];
     int method_count;
+    CustomFunc constructor;     // __init__ konstruktor
+    int has_constructor;
 } CustomClass;
 
-// Új: Egy konkrét objektum példány
+// Objektum példány
 typedef struct {
     char name[NAME_LEN];
     char class_name[NAME_LEN];
-    double local_vars[10]; // Objektum-szintű adatok
+    CustomVar local_vars[50];   // Objektum lokális változók
+    int local_var_count;
 } CustomObject;
 
+// Import struktúra
+typedef struct {
+    char module_name[NAME_LEN];
+    int loaded;
+} ImportedModule;
+
+// Fő állapot
 typedef struct {
     BaseState info;
     CustomVar vars[MAX_VARS];
     int var_count;
     CustomClass classes[MAX_CLASSES];
     int class_count;
-    CustomObject objects[MAX_OBJECTS]; // Objektum tároló
+    CustomObject objects[MAX_OBJECTS];
     int object_count;
+    CustomFunc functions[MAX_FUNCS];
+    int func_count;
+    ImportedModule imports[MAX_IMPORTS];
+    int import_count;
 } EngineState;
-
-
 
 typedef void (*logic_update_fn)(EngineState* state);
 
